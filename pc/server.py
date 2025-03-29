1c11ff7551e4083375610fb9bb6476acbc1d5e25
BuildTools <mikulas.vanecek@gmail.com>
2025-03-29 21:20:13 +0100

    Changed the format of IMU data to one compatible with OpenTrack

--- pc/server.py
+++ pc/server.py
@@ -1,14 +1,13 @@
 import socket
 import struct
 
-# Set up UDP listener
-UDP_PORT = 5555
+PORT = 5555
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", UDP_PORT))
+sock.bind(("0.0.0.0", PORT))
 
 print("Listening for IMU data...")
 
 while True:
-    data, _ = sock.recvfrom(1024)  # Receive data
-    yaw, roll, pitch = struct.unpack("fff", data)  # Unpack the binary message
+    data, _ = sock.recvfrom(1024)
+    yaw, roll, pitch = struct.unpack("fff", data)
     print(f"Yaw: {yaw:.2f}, Roll: {roll:.2f}, Pitch: {pitch:.2f}")
