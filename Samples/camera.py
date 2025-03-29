a7b682c988df0a2ed60af37800397141d1b6d7ab
BuildTools <mikulas.vanecek@gmail.com>
2025-03-29 13:54:36 +0100

    Fixed swapped color channels in camera.py

--- Samples/camera.py
+++ Samples/camera.py
@@ -6,12 +6,19 @@ picam2 = Picamera2()
 picam2.start()
 
 while True:
+    # Capture the frame
     frame = picam2.capture_array()
 
-    cv2.imshow("Webcam", frame)
+    # Convert to RGB (sRGB) if necessary
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Display the image in a window
+    cv2.imshow("Webcam", frame_rgb)
+
+    # Exit loop on pressing 'q'
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+# Stop the camera and close OpenCV windows
 picam2.stop()
 cv2.destroyAllWindows()
