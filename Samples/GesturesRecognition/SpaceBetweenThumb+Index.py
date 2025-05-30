3ac0bfbe9f0cb6c50fa06286d3d66a7242e6c7bf
Miky8745 <mikulas.vanecek@gmail.com>
2025-05-30 20:41:16 +0200

    Refactored SpaceBetweenThumb+Index.py

--- Samples/GesturesRecognition/SpaceBetweenThumb+Index.py
+++ Samples/GesturesRecognition/SpaceBetweenThumb+Index.py
@@ -2,7 +2,6 @@ import cv2
 import mediapipe as mp
 import math
 
-# Inicializace MediaPipe
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
@@ -22,31 +21,25 @@ while cap.isOpened():
         for hand_landmarks in results.multi_hand_landmarks:
             h, w, _ = image.shape
 
-            def get_point(id):
-                lm = hand_landmarks.landmark[id]
+            def get_point(identifier):
+                lm = hand_landmarks.landmark[identifier]
                 return int(lm.x * w), int(lm.y * h)
 
-            # Body: špička palce (4), špička ukazováčku (8)
             thumb_tip = get_point(mp_hands.HandLandmark.THUMB_TIP)
             index_tip = get_point(mp_hands.HandLandmark.INDEX_FINGER_TIP)
 
-            # Délka článku ukazováčku: mezi klouby 6 (MCP) a 8 (TIP)
             index_mcp = get_point(mp_hands.HandLandmark.INDEX_FINGER_MCP)
             index_length = math.hypot(index_tip[0] - index_mcp[0], index_tip[1] - index_mcp[1])
 
-            # Vzdálenost mezi palcem a ukazováčkem
             thumb_index_dist = math.hypot(index_tip[0] - thumb_tip[0], index_tip[1] - thumb_tip[1])
 
-            # Zoom metriku
             if index_length > 0:
                 zoom_unit = thumb_index_dist / index_length
             else:
                 zoom_unit = 0
 
-            # Přibližný odhad v centimetrech (předpokládáme, že článek má cca 5 cm)
             real_distance_cm = (thumb_index_dist / index_length) * 5
 
-            # Vykreslení
             cv2.line(image, thumb_tip, index_tip, (0, 255, 255), 3)
             cv2.circle(image, thumb_tip, 10, (255, 0, 0), -1)
             cv2.circle(image, index_tip, 10, (255, 0, 0), -1)
@@ -54,7 +47,7 @@ while cap.isOpened():
             cv2.putText(image, f"Zoom unit: {zoom_unit:.2f}", (10, 50),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-            cv2.putText(image, f"Odhad vzdalenosti: {real_distance_cm:.1f} cm", (10, 90),
+            cv2.putText(image, f"Distance: {real_distance_cm:.1f} cm", (10, 90),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
 
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
