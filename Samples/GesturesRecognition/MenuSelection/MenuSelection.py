a9ad82c1c0fdd958b4b59f276c63ada7b7c77ead
Miky8745 <mikulas.vanecek@gmail.com>
2025-05-30 20:50:46 +0200

    Refactored MenuSelection.py

--- Samples/GesturesRecognition/MenuSelection/MenuSelection.py
+++ Samples/GesturesRecognition/MenuSelection/MenuSelection.py
@@ -17,22 +17,16 @@ while cap.isOpened():
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            # Kreslení základních linií ruky
             mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            # Získání pozic jednotlivých bodů na ukazováčku
             index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
             wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
 
-            # Porovnání pozice ukazováčku s zápěstím
-            if index_finger_tip.x < wrist.x:  # Pokud je ukazováček vlevo od zápěstí
-                # Zjištění výšky ukazováčku
+            if index_finger_tip.x < wrist.x:
                 y_position = int(index_finger_tip.y * img.shape[0])
 
-                # Nakreslení čáry na tuto výšku
                 cv2.line(img, (0, y_position), (img.shape[1], y_position), (0, 255, 0), 2)
 
-    # Zobrazení výsledného obrázku
     cv2.imshow("Hand Tracking", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
